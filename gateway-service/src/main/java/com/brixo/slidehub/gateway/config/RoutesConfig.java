3e4bbd3ee37a9892c0cde7ffa95137d0bbf93e0c
Mike <69970540+mikerb95@users.noreply.github.com>
2026-03-30 10:01:35 -0500

    refactor: Organize imports in RoutesConfig for better readability

--- gateway-service/src/main/java/com/brixo/slidehub/gateway/config/RoutesConfig.java
+++ gateway-service/src/main/java/com/brixo/slidehub/gateway/config/RoutesConfig.java
@@ -6,8 +6,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.web.servlet.function.RequestPredicates;
 import org.springframework.web.servlet.function.RouterFunction;
+import org.springframework.web.servlet.function.ServerRequest;
 import org.springframework.web.servlet.function.ServerResponse;
 
+import java.util.function.Function;
+
+import static org.springframework.cloud.gateway.server.mvc.filter.BeforeFilterFunctions.addRequestHeadersIfNotPresent;
 import static org.springframework.cloud.gateway.server.mvc.filter.FilterFunctions.uri;
 import static org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctions.route;
 import static org.springframework.cloud.gateway.server.mvc.handler.HandlerFunctions.http;
