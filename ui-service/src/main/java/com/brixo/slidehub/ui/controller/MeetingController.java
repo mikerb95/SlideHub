18fda8bda493e214d89095be7be2a42bc7afcfe8
Mike <69970540+mikerb95@users.noreply.github.com>
2026-03-29 22:48:19 -0500

    refactor: Remove unused Optional import in MeetingController

--- ui-service/src/main/java/com/brixo/slidehub/ui/controller/MeetingController.java
+++ ui-service/src/main/java/com/brixo/slidehub/ui/controller/MeetingController.java
@@ -17,7 +17,6 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.Map;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/presentations/{presentationId}/meeting")
