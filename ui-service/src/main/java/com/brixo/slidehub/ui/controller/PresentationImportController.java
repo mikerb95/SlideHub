326e6e17a823b9adbf944f8260fe432023beb84f
Mike <69970540+mikerb95@users.noreply.github.com>
2026-03-31 01:18:12 -0500

    feat: add DriveContents import in PresentationImportController for improved functionality

--- ui-service/src/main/java/com/brixo/slidehub/ui/controller/PresentationImportController.java
+++ ui-service/src/main/java/com/brixo/slidehub/ui/controller/PresentationImportController.java
@@ -1,5 +1,6 @@
 package com.brixo.slidehub.ui.controller;
 
+import com.brixo.slidehub.ui.model.DriveContents;
 import com.brixo.slidehub.ui.model.DriveFile;
 import com.brixo.slidehub.ui.model.DriveFolder;
 import com.brixo.slidehub.ui.model.Presentation;
