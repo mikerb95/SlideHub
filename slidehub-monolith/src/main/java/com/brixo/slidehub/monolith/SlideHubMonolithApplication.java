b6fda833060e4fdaced7cc17a3a4ee71bb192d2b
Mike <69970540+mikerb95@users.noreply.github.com>
2026-04-04 02:19:56 -0500

    fix: Update import statement for EntityScan annotation in SlideHubMonolithApplication

--- slidehub-monolith/src/main/java/com/brixo/slidehub/monolith/SlideHubMonolithApplication.java
+++ slidehub-monolith/src/main/java/com/brixo/slidehub/monolith/SlideHubMonolithApplication.java
@@ -1,10 +1,10 @@
 package com.brixo.slidehub.monolith;
 
 import com.brixo.slidehub.monolith.ratelimit.GatewayRateLimitProperties;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.persistence.autoconfigure.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
