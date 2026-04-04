5f2466ca58ad1ed8e87573b54274f917607662c9
Mike <69970540+mikerb95@users.noreply.github.com>
2026-04-04 00:52:31 -0500

    feat: Add JPA and MongoDB repository support to SlideHubMonolithApplication

--- slidehub-monolith/src/main/java/com/brixo/slidehub/monolith/SlideHubMonolithApplication.java
+++ slidehub-monolith/src/main/java/com/brixo/slidehub/monolith/SlideHubMonolithApplication.java
@@ -1,14 +1,20 @@
 package com.brixo.slidehub.monolith;
 
 import com.brixo.slidehub.monolith.ratelimit.GatewayRateLimitProperties;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication(scanBasePackages = "com.brixo.slidehub")
 @EnableScheduling
 @EnableConfigurationProperties(GatewayRateLimitProperties.class)
+@EnableJpaRepositories(basePackages = "com.brixo.slidehub.ui.repository")
+@EnableMongoRepositories(basePackages = "com.brixo.slidehub.ai.repository")
+@EntityScan(basePackages = "com.brixo.slidehub.ui.model")
 public class SlideHubMonolithApplication {
 
     public static void main(String[] args) {
