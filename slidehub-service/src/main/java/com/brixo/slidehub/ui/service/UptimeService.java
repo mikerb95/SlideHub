084c33f37f667f4767d3b9cc1270bfc31c420c6c
Mike <69970540+mikerb95@users.noreply.github.com>
2026-04-06 13:49:20 -0500

    Remove unused import for MediaType in UptimeService

--- slidehub-service/src/main/java/com/brixo/slidehub/ui/service/UptimeService.java
+++ slidehub-service/src/main/java/com/brixo/slidehub/ui/service/UptimeService.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.databind.JsonNode;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
